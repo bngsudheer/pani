85efd211391b19ca27bdb27df789dc25af733207
sudheer <none@none>
2014-07-03 22:03:56 +0530

    user layout

---
+++ pani/doc/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY ASC,
+    username TEXT NOT NULL,
+    password TEXT NULL,
+    public_key TEXT NULL
+);
+
+
+
+/* Default data
+*/
+INSERT INTO users (username, password) VALUES ('admin', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86');
+
+
